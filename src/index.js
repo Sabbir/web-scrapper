6677fd3424de6fb894d8428fdc6d8f487b91ef27
unknown <sabbirhs@yahoo.com>
2022-11-09 10:26:56 +0600

    reactjs design

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      
+      </Routes>
+    </Layout>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
